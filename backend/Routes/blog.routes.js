50b3e82d3e32a72e8e5b65e5c9f4b5fd1f2902fb
cloud-009 <rpoudel750@gmail.com>
2022-12-16 16:13:45 +0530

    first commit

--- backend/Routes/blog.routes.js
+++ backend/Routes/blog.routes.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const blogRoute = express.Router();
 
+const blogModel = require('../Model/post');
+
 // Get all data
-blogRoute.route('/blog').get((req, res, next) =>{
-      
-})
+blogRoute.route('/').get((req, res, next) => {
+    blogModel.find((err, data) => {
+        if (err) return next(err);
+        else res.json(data);
+    })
+});
+
+// Post the data
+blogRoute.route('/add-blog').post((req, res, next) => {
+    blogModel.create(req.body, (err, data) => {
+        if (err) return next(err);
+        else res.json(data);
+    });
+});
+
+module.exports = blogRoute;
---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    mongodb = require('./Database/db');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongodb.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected successfully');
+}).catch((err) => {
+    console.log('Error occured while connecting')
+    console.log(err);
+});
+
+const blogRoute = require('./Routes/blog.routes');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/api', blogRoute);
+
+const port = 3500;
+
+app.listen(port, () => {
+    console.log('Running on port', port);
+})
+
+app.get('/', (req, res) => {
+    res.send('invalid routes');
+});
+
+// error handling 
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
